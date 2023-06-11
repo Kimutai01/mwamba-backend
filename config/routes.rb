ed75db92dc0cb60ef9677cc49351976bb00a898b
MICHAELMUNAVU83 <michaelmunavu83.com>
2023-06-11 20:36:44 +0300

    Added blogs

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
+  resources :blogs
   # config/routes.rb
   namespace :api do
+
     namespace :v1 do
+      resources :blogs, only: [:index, :show, :create, :update, :destroy]
       resources :users, only: [:create]
       post '/login', to: 'auth#create'
       get '/profile', to: 'users#profile'
