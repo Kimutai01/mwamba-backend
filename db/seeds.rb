0f2dd77645a5ba3f411f26bdfc917803f824c362
MICHAELMUNAVU83 <michaelmunavu83.com>
2023-06-27 16:33:40 +0300

    Added

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,9 +14,12 @@ blogs = Blog.create([
 
 
 products = Product.create([
-    {name: "Home Jersey", photo: "https://res.cloudinary.com/dakiak4mc/image/upload/v1687665209/chino-rocha-v_sciNJhbnM-unsplash_fsmr8t.jpg", rotating_images: ["https://res.cloudinary.com/dakiak4mc/image/upload/v1687665209/chino-rocha-v_sciNJhbnM-unsplash_fsmr8t.jpg", "https://res.cloudinary.com/dakiak4mc/image/upload/v1687665209/chino-rocha-v_sciNJhbnM-unsplash_fsmr8t.jpg"]},
-    {name: "Away Jersey", photo: "https://res.cloudinary.com/dakiak4mc/image/upload/v1687665209/chino-rocha-v_sciNJhbnM-unsplash_fsmr8t.jpg", rotating_images: ["https://res.cloudinary.com/dakiak4mc/image/upload/v1687665209/chino-rocha-v_sciNJhbnM-unsplash_fsmr8t.jpg", "https://res.cloudinary.com/dakiak4mc/image/upload/v1687665209/chino-rocha-v_sciNJhbnM-unsplash_fsmr8t.jpg"]},
-    {name: "Hat", photo: "https://res.cloudinary.com/dakiak4mc/image/upload/v1687665209/chino-rocha-v_sciNJhbnM-unsplash_fsmr8t.jpg", rotating_images: ["https://res.cloudinary.com/dakiak4mc/image/upload/v1687665209/chino-rocha-v_sciNJhbnM-unsplash_fsmr8t.jpg", "https://res.cloudinary.com/dakiak4mc/image/upload/v1687665209/chino-rocha-v_sciNJhbnM-unsplash_fsmr8t.jpg"]}
+
+    {name: "Home Jersey", photo: "https://res.cloudinary.com/dakiak4mc/image/upload/v1687872349/team2_l7ibk8.png", rotating_images: ["https://res.cloudinary.com/dakiak4mc/image/upload/v1687872358/team3_xfakyq.png", "https://res.cloudinary.com/dakiak4mc/image/upload/v1687872369/team4_kv2vud.png", "https://res.cloudinary.com/dakiak4mc/image/upload/v1687872316/team5_dqaa8x.png"]},
+   
+    {name: "Away Jersey", photo: "https://res.cloudinary.com/dakiak4mc/image/upload/v1687871015/MWAMBA_RUGBY_-_FRONT_sogptt.png", rotating_images: ["https://res.cloudinary.com/dakiak4mc/image/upload/v1687871015/MWAMBA_RUGBY_-_FRONT_sogptt.png", "https://res.cloudinary.com/dakiak4mc/image/upload/v1687871040/MWAMBA_RUGBY_-_BACK_spbtbi.png", "https://res.cloudinary.com/dakiak4mc/image/upload/v1687871027/MWAMBA_RUGBY_-_FRONT_LEFT_vzton9.png"]},
+  
+    {name: "Reversible Bucket Hat", photo: "https://res.cloudinary.com/dakiak4mc/image/upload/v1687870655/WhatsApp_Image_2023-06-26_at_9.59.53_PM_mpixts.jpg", rotating_images: ["https://res.cloudinary.com/dakiak4mc/image/upload/v1687870666/WhatsApp_Image_2023-06-26_at_9.59.54_PM_1_wllxtv.jpg", "https://res.cloudinary.com/dakiak4mc/image/upload/v1687870667/WhatsApp_Image_2023-06-26_at_9.59.55_PM_rxlu5c.jpg" ,"https://res.cloudinary.com/dakiak4mc/image/upload/v1687870655/WhatsApp_Image_2023-06-26_at_9.59.53_PM_mpixts.jpg"]}
 
 
 ])
