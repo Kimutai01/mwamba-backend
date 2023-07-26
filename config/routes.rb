60d14450cd974931e7b060f676d8b0b49a61db27
MICHAELMUNAVU83 <michaelmunavu83.com>
2023-07-26 16:11:43 +0300

    Added

--- config/routes.rb
+++ config/routes.rb
@@ -13,9 +13,11 @@ Rails.application.routes.draw do
     namespace :v1 do
       resources :blogs, only: [:index, :show, :create, :update, :destroy]
       resources :users, only: [:create]
+      resources :fixtures, only: [:index, :show, :create, :update, :destroy]
       resources :cart_items, only: [:index, :show, :create, :update, :destroy]
       resources :products, only: [:index, :show, :create, :update, :destroy] do
         resources :categories, only: [:index, :show, :create, :update, :destroy]
+
       end
 
       resources :members, only: [:index, :show, :create, :update, :destroy]
